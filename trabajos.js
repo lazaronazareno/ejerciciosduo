e38fd2bda30888e3fad6fcb279f7bc5be5089c31
sempai3 <mangrix16@gmail.com>
2020-02-10 15:18:39 -0300

    segundoejercicio

--- trabajos.js
+++ trabajos.js
@@ -1,10 +1,20 @@
+//*//////////
+//function pordos (lista) {
+// let resultado=[];
+//for (let i = 0 ; i < lista.length; i++) {
+//    resultado[i] = i * 2  ;
+//}
+//console.log (resultado)
+// 
+//}
+//
+//
 
-function pordos (lista) {
- let resultado=[];
-for (let i = 0 ; i < lista.length; i++) {
-    resultado[i] = i * 2  ;
-}
-console.log (resultado)
+
+function dma ({fecha}) {
  
-}
+    let formato = {dia : fecha.dia, mes : fecha.mes , año : fecha.año}
+    console.log(formato.dia + "-" + formato.mes + "-" + formato.año)
 
+
+   }
